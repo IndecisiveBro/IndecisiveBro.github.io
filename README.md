e7b43052232e41542dc4871458cd80ff3ace059b
IndecisiveBro <153704188+IndecisiveBro@users.noreply.github.com>
2023-12-13 01:12:10 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+<h1 align="center">Indecisive's Discord Bio Made by iamkoji</h1>
+<p align="center">
+
+This is a personal website made for a Indecisive.
+
+
+### Credits below:
+
+Phineas for the discord display and background:
+
+- ✅ https://github.com/theosanct0s/discord.bio!
+
+iamkoji for the music, click to enter screen, and small additions like rainbow text etc.
---
+++ assets/js/config.js
@@ -0,0 +1,35 @@
+const BADGES = [
+  {
+    name: "HypeSquad Brilliance", // Message that will appear when hovering over the badge
+    image: "assets/badges/hypesquad_brilliance.svg",
+  },
+  {
+    name: "Subscriber since Aug 26, 2023",
+    image: "assets/badges/nitro.svg",
+  },
+  {
+    name: "Server boosting Dec 8, 2023",
+    image: "assets/badges/boost.svg",
+  }
+];
+
+const USER_ID = "927088334830968853"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
+const BACKGROUND_IMAGE = "assets/images/bg.png"; // Here is the background of the site, behind your profile
+const STATUS = {
+  dnd: {
+    name: "Do not disturb", // Discord Status
+    color: "#ed4245",
+  },
+  idle: {
+    name: "Busy",
+    color: "#faa81a",
+  },
+  online: {
+    name: "Online",
+    color: "#3ba55d",
+  },
+  offline: {
+    name: "Offline",
+    color: "#6e7985",
+  },
+};
